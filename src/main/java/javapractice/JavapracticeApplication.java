0c79229bf4504ffdb2f11a605dbf06c59ce4c498
Yul Jeon <dalbitstream@gmail.com>
2024-08-11 01:45:02 +0900

    Initial commit

---
+++ src/main/java/javapractice/JavapracticeApplication.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapracticeApplication.class, args);
+	}
+
+}
